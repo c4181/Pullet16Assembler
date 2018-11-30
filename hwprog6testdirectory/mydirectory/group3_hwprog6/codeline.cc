81bd34cbff686a81b5b291e158504f0a66e08901
Christopher <cm4181@gmail.com>
2018-11-30 14:35:31 -0500

    Added logic for GetAddr

--- hwprog6testdirectory/mydirectory/group3_hwprog6/codeline.cc
+++ hwprog6testdirectory/mydirectory/group3_hwprog6/codeline.cc
@@ -32,11 +32,7 @@ CodeLine::~CodeLine() {
  * Accessor for the 'addr_'.
 **/
 string CodeLine::GetAddr() const {
-  string returnvalue;
-
-  // code goes here
-  
-  return returnvalue;
+  return addr_;
 }
 
 /***************************************************************************
